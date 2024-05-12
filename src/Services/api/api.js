6aede186cbdadde4a292bdcf23461156eaeb3230
pajarmuharam14 <pajarmuharam321@gmail.com>
2024-05-12 13:29:35 +0700

    refactor vite config

--- src/Services/api/api.js
+++ src/Services/api/api.js
@@ -1,5 +1,5 @@
 export const getAnimeResponse = async (resource, query) => {
-  const response = await fetch(`https://api.jikan.moe/v4/${resource}?${query}`);
+  const response = await fetch(`${import.meta.env.VITE_API_KEY_BASE_URL}/${resource}?${query}`);
   const anime = await response.json();
   return anime;
 };
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
+// const process = dotenv();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    "process.env.VITE_API_KEY_BASE_URL": JSON.stringify(process.env.VITE_API_KEY_BASE_URL),
+  },
+});
