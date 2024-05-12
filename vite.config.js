95ec0e66d6741e2d781b004172fdeff819f360c9
pajarmuharam14 <pajarmuharam321@gmail.com>
2024-05-13 06:45:52 +0700

    refactor vite config and .env file

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
 
-const process = dotenv();
+// const process = dotenv();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env.VITE_API_KEY_BASE_URL": JSON.stringify(process.env.VITE_API_KEY_BASE_URL),
-  },
 });
